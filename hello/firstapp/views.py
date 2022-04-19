a3a817666e5eff2a82b93265ae526a2a0fe40c2c
YOUR_NAME <YOUR_EMAIL>
2022-04-19 18:12:53 +0300

    1st

---
+++ hello/firstapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def products(request, productid):
+    output = f'<h2>Продукт № {productid}</h2>'
+    return HttpResponse(output)
+
+
+def users(request, id, name):
+    output = f'<h2>Пользователь</h2><h3>id: {id} Имя: {name}</hЗ>'
+    return HttpResponse(output)
+
+
+def index(request):
+    someList = [1, 2, 3, 4]
+    data = {"someList": someList}
+    return render(request, 'index.html', context=data)
